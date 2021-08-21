d7538fa4b2ea60dd6029e91b353283c632d2b9cd
Brandon Reyes Alvarado <67602859+Brandon0309@users.noreply.github.com>
2021-08-21 17:17:02 -0600

    Primary operation added
    The total amount and the variables are integrated to the project, asked by an external client that connects, and returned as a total amount.

--- src/ClasesYObjetos/Main.java
+++ src/ClasesYObjetos/Main.java
@@ -1,8 +1,42 @@
 package ClasesYObjetos;
+import java.net.*;
+import java.io.*;
 
 public class Main {
+    static Socket cliente;
+    static int puerto = 9000;
+    static String ip = "127.0.0.1";
+    static BufferedReader entrada;
+    static BufferedReader teclado;
+    static PrintStream salida;
 
     public static void main(String[] args) {
+        try {
+
+
+            conector c = new conector();
+            c.iniciar();
+
+
+            /*
+            cliente = new Socket(ip, puerto);
+            entrada = new BufferedReader
+                    (new InputStreamReader(cliente.getInputStream()));
+            teclado = new BufferedReader(new InputStreamReader(System.in));
+            String tec = teclado.readLine();
+            salida = new PrintStream(cliente.getOutputStream());
+            salida.println(tec);
+            String msg = entrada.readLine();
+            System.out.println(msg);
+
+            entrada.close();
+            salida.close();
+            teclado.close();
+            cliente.close();
+            */
+
+        }catch (Exception e){};
+
 
 
     }
--- src/ClasesYObjetos/conector.java
+++ src/ClasesYObjetos/conector.java
@@ -1,4 +1,6 @@
 package ClasesYObjetos;
+import jdk.internal.util.xml.impl.Input;
+
 import java.net.*;
 import java.io.*;
 
@@ -6,18 +8,54 @@ import java.io.*;
 public class conector {
 
     ServerSocket server;
-    Socket socket;
+    Socket socketSocket;
     int puerto = 9000;
-    DataOutputStream salida;
-    BufferedReader entrada;
+    DataOutputStream socketSalida;
+    BufferedReader socketEntrada;
+    /*Socket cliente;
+    int puerto = 9000;
+    String ip = "127.0.0.1";
+    BufferedReader entrada, teclado;
+    PrintStream salida; */
+
 
     public void iniciar() {
-        try{
-            server = new ServerSocket(puerto);
-            socket = new Socket();
-            socket = server.accept();
 
-            entrada = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        try{
+           /*
+            cliente = new Socket(ip, puerto);
+            entrada = new BufferedReader
+                    (new InputStreamReader(cliente.getInputStream()));
+            teclado = new BufferedReader(new InputStreamReader(System.in));
+            String tec = teclado.readLine();
+            salida = new PrintStream(cliente.getOutputStream());
+            salida.println(tec);
+            String msg = entrada.readLine();
+            System.out.println(msg);
+
+            entrada.close();
+            salida.close();
+            teclado.close();
+            cliente.close();
+*/
+
+            server = new ServerSocket(puerto);
+            socketSocket = new Socket();
+            socketSocket = server.accept();
+
+            socketEntrada = new BufferedReader (new InputStreamReader(socketSocket.getInputStream()));
+            int valor = Integer.parseInt(socketEntrada.readLine());
+            int peso = Integer.parseInt(socketEntrada.readLine());
+            int porcentaje = Integer.parseInt(socketEntrada.readLine());
+
+            System.out.println("El valor del producto es de " + valor +
+                               " el peso es de " + peso +
+                               " y el impuesto aplicado es de " + porcentaje);
+
+            socketSalida = new DataOutputStream(socketSocket.getOutputStream());
+            double montoTotal = (valor*porcentaje/100) + (peso*0.15);
+            socketSalida.writeUTF("El monto total seria de: " + montoTotal);
+            socketSocket.close();
 
         }catch (Exception e){};
 
